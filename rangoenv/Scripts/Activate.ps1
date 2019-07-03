03ab50a29384d58f11e14384dfafe7afa09de86d
Bob Koense <bobkoense@hotmail.com>
2019-07-04 00:56:26 +0200

    added static

--- rangoenv/Scripts/Activate.ps1
+++ rangoenv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\bobko\tango\rangoenv"
+$env:VIRTUAL_ENV="C:\Users\bobko\rango\rangoenv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
